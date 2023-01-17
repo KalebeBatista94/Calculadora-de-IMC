6e46b75a50f0dbf7d6ecba97415df00fa79bfa2f
KalebeBatista94 <kalebe474@gmail.com>
2023-01-17 11:15:50 -0300

    improving the application

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// const inputIdade = document.getElementById('inputIdade');
 const inputAltura = document.getElementById('inputAltura');
 const inputPeso = document.getElementById('inputPeso');
 const calcular = document.getElementById('calcular');
+const textArea = document.getElementById('text-area');
+const loadingImage = document.getElementById('loading-image');
 
 inputAltura.addEventListener('keyup', (e) => { // Substituindo vírgula por ponto
     const el = e.target
@@ -13,30 +14,40 @@ inputAltura.addEventListener('keyup', (e) => { // Substituindo vírgula por pont
 calcular.addEventListener('click', () => {
     const result = inputPeso.value / (Math.pow(inputAltura.value,2));
 
+    let text;
+
     if(result < 18.5){
-        alert(` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC está abaixo do recomendado para a sua altura.`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC está abaixo do recomendado para a sua altura.`;
     }
 
     if(result >= 18 && result <= 24.9){
-        alert(` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC é considerado normal para a sua altura.`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC é considerado normal para a sua altura.`;
     }
 
     if(result >= 25 && result <= 29.9){
-        alert(` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Você presica praticar exercícios!`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, De acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Você presica praticar exercícios!`;
     }
 
     if(result >= 30 && result <= 34.9){
-        alert(` Seu IMC é de ${result.toFixed(2)}, Obesidade grau I, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, Obesidade grau I, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`;
     }
 
     if(result >= 35 && result <= 40 ){
-        alert(` Seu IMC é de ${result.toFixed(2)}, Obesidade grau II, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, Obesidade grau II, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`;
     }
 
     if(result > 40 ){
-        alert(` Seu IMC é de ${result.toFixed(2)}, Obesidade grau III, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`);
+        text = ` Seu IMC é de ${result.toFixed(2)}, Obesidade grau III, de acordo com a Organização Mundial da Saúde, seu IMC está acima do recomendado para a sua altura. Tenha uma vida saudável, pratique exercícios!`;
     }
+   
+    loadingImage.style.display = 'unset'
+
+    setInterval(() => {
+        loadingImage.style.display = 'none'
+        textArea.innerHTML = text;
+        textArea.style.display = 'unset';
+    }, 2000);
+
 
 })
 
-console.log(Math.pow(inputAltura,2))
